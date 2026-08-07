a8ecaa0f60e3e2d6992282b3cfab29a70dfc1359
anshul200527 <anshulbahuguna27@gmail.com>
2026-08-07 12:04:18 +0530

    feat(theme): update theme provider to disable system theme switching and enforce light theme

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -11,7 +11,8 @@ export function ThemeProvider({
     <NextThemesProvider
       attribute="class"
       defaultTheme="light"
-      enableSystem
+      forcedTheme="light"
+      enableSystem={false}
     >
       {children}
     </NextThemesProvider>
